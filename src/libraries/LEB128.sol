503250e6c32f571447cbf8692f25797f3a299121
Sunur Efe Vural <efe@efe.kim>
2023-12-01 18:23:06 +0300

    update LEB128 lib

--- src/libraries/LEB128.sol
+++ src/libraries/LEB128.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @author shung: https://github.com/Shungy/leb128-nooffset/blob/063c70211eea731736cd459a2037a0dce711cc8b/src/LEB128Lib.sol
 library LEB128 {
-    /// @dev Decodes an Unsigned LEB128 encoded value, starting from calldata `ptr`.
-    /// See: https://en.wikipedia.org/wiki/LEB128#Decode_unsigned_integer
+    /// @dev Decodes an unsigned LEB128 encoded value, starting from calldata `ptr`.
+    /// See https://en.wikipedia.org/wiki/LEB128#Decode_unsigned_integer.
     /// Note: Anything overflowing 256 bits is truncated silently without a revert.
-    /// Note: Superfluous zero padding can be used to control the length of the encoded data.
+    /// Note: Superfluous zero padding can be used to inflate the length of the encoded data.
     function rawDecodeUint(uint256 ptr) internal pure returns (uint256 result, uint256 newPtr) {
         /// @solidity memory-safe-assembly
         assembly {
@@ -14,7 +13,8 @@ library LEB128 {
                 let nextByte := byte(0, calldataload(ptr))
                 result := or(result, shl(shift, and(nextByte, 0x7f)))
                 ptr := add(ptr, 1)
-                if iszero(shr(7, nextByte)) { break }
+                if shr(7, nextByte) { continue }
+                break
             }
             newPtr := ptr
         }
